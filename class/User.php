b8719d99db081461a95d06e671b18aa3cf20b945
Bruno Vasconcellos <brunovasconcellos133@gmail.com>
2019-09-18 19:30:32 -0300

    first commit

--- class/User.php
+++ class/User.php
@@ -69,6 +69,52 @@ class User {
 
     }
 
+    public static function getList(){
+
+        $sql = new Sql();
+
+        return $sql->select("SELECT * FROM tb_usuario ORDER BY deslogin;");
+
+    }
+
+    public static function search($login){
+
+        $sql = new Sql();
+
+        return $sql->select("SELECT * FROM tb_usuario WHERE deslogin LIKE :SEARCH ORDER BY deslogin", array(
+
+            ":SEARCH"=>"%".$login."%"
+
+        ));
+
+    }
+
+    public function login($login, $password){
+
+        $sql = new Sql();
+
+        $results = $sql->select("SELECT * FROM tb_usuario WHERE deslogin = :LOGIN AND dessenha = :PASSWORD", array(
+            ":LOGIN"=> $login,
+            ":PASSWORD"=>"$password"
+        ));
+
+        if (count($results) > 0){
+
+            $row = $results[0];
+
+            $this->setIdusuario($row['idusuario']);
+            $this->setDeslogin($row['deslogin']);
+            $this->setDessenha($row['dessenha']);
+            $this->setDtcadastro(new DateTime($row['dtcadastro']));
+
+        }else{
+
+            throw new Exception("Nome de usuário e/ou senha invalidos.");
+
+        }
+
+    }
+
     public function __toString(){
        return json_encode(array(
            "idusuario"=>$this->getIdusuario(),
--- index.php
+++ index.php
@@ -9,9 +9,33 @@ $usuario = $sql->select("SELECT * FROM tb_usuario");
 echo json_encode($usuario);
 */
 
+/* carrega um usuario
 $user = new User();
 
 $user->loadById(6);
+
+echo $user;
+*/
+
+/*carrega uma lista
+
+$list = User::getList();
+
+echo json_encode($list);
+*/
+
+/*carrega uma lista pelo login
+
+$search = User::search("re");
+
+echo json_encode($search);
+*/
+
+//carrega um usuario usando login e senha
+
+$user = new User();
+
+$user->login("ryuji", "217362");
 
 echo $user;
 
